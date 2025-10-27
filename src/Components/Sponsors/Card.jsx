6a1b3d702a14495c7302a3a7ffd1c5e83b49e8d9
di35117 <dishankchoudhury@gmail.com>
2025-10-27 17:50:44 +0530

    Refactor Card component layout and structure

--- src/Components/Sponsors/Card.jsx
+++ src/Components/Sponsors/Card.jsx
@@ -1,39 +1,32 @@
-import React from "react";
-import "./Card.css"; // import the raw CSS file
-
-import firstPokemon from "../../../public/images/pokeballs.svg";
-import pokeball1 from "../../../public/images/pokeball1.svg";
+import firstPokemon from "../../../public/images/pokeball.svg";
+import pokeball1 from "../../../public/images/pokeball1.svg"
 import pokeball2 from "../../../public/images/pokeball2.svg";
 import pokeball3 from "../../../public/images/pokeball3.svg";
-
 const Card = ({ logoImage, sponsorName }) => {
   return (
-    <div className="cardMain bg-blue-900 rounded-xl shadow-lg w-full max-w-56">
-      <div className="relative bg-red-600 rounded-lg flex flex-col gap-0.5 aspect-7/10 innerCard">
-        {/* Top white header */}
-        <div className="bg-white rounded-t-md flex items-center space-x-1 topBar">
-          <div className="bg-white rounded-full pokeballWrap">
-            <img src={firstPokemon} className="w-4 h-4 object-contain" />
+    <div className="bg-blue-900 p-2 rounded-xl shadow-lg w-full max-w-56 mx-auto">
+      <div className="relative bg-red-600 rounded-lg pb-2.5 flex flex-col gap-0.5 aspect-7/10">
+        <div className="bg-white rounded-t-md p-0.5 flex items-center space-x-1">
+          <div className="bg-white rounded-full p-0.5">
+            <img src={firstPokemon} alt="" className="w-4 h-4 object-contain" />
           </div>
-          <div className="bg-white rounded-full pokeballWrap">
-            <img src={pokeball1} className="w-4 h-4 object-contain" />
+          <div className="bg-white rounded-full p-0.5">
+            <img src={pokeball1} alt="" className="w-4 h-4 object-contain" />
           </div>
-          <div className="bg-white rounded-full pokeballWrap">
-            <img src={pokeball2} className="w-4 h-4 object-contain" />
+          <div className="bg-white rounded-full p-0.5">
+            <img src={pokeball2} alt="" className="w-4 h-4 object-contain" />
           </div>
-          <div className="bg-white rounded-full pokeballWrap">
-            <img src={pokeball3} className="w-4 h-4 object-contain" />
+          <div className="bg-white rounded-full p-0.5">
+            <img src={pokeball3} alt="" className="w-4 h-4 object-contain" />
           </div>
         </div>
 
-        {/* Middle logo box */}
-        <div className="bg-white rounded-md grow flex items-center justify-center mx-2.5 border-2 border-black logoBox">
-          <img src={logoImage} className="object-contain h-16 sm:h-30" />
+        <div className="bg-white rounded-md grow flex items-center justify-center p-2 mt-1 md:mt-2 sm:p-4 mx-2.5 border-2 border-black">
+          <img src={logoImage} alt="" className="object-contain h-16 sm:h-30" />
         </div>
 
-        {/* Yellow sponsor box */}
-        <div className="bg-yellow-400 rounded-md text-center h-22 sm:h-20 mx-2.5 border-2 border-black sponsorBox">
-          <p className="font-pocket-monk text-black text-2xl sm:text-3xl sponsorName">
+        <div className="bg-yellow-400 rounded-md p-0.5 text-center h-22 sm:h-20 mx-2.5 border-2 border-black ">
+          <p className="font-pocket-monk text-black text-2xl sm:text-3xl pt-2">
             {sponsorName}
           </p>
           <p className="text-[0.6rem] sm:text-xs text-black font-courier font-space">
@@ -41,14 +34,13 @@ const Card = ({ logoImage, sponsorName }) => {
           </p>
         </div>
 
-        {/* Bottom Pokémon icon */}
         <img
           src={firstPokemon}
+          alt=""
           className="absolute bottom-18 sm:bottom-[59px] left-1/2 -translate-x-1/2 -translate-y-1/2 w-8 h-8 sm:w-10 sm:h-10"
         />
       </div>
     </div>
   );
 };
-
 export default Card;
