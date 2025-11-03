58b430ea37b2553df5e2c3771f81e12fc90a02a9
Dhruv Kumar <dhruv.k.dev@gmail.com>
2025-11-03 19:46:24 +0530

    teams page minor fixes

--- src/Components/TeamCard/TeamCard.jsx
+++ src/Components/TeamCard/TeamCard.jsx
@@ -10,8 +10,8 @@ export default function HackCard({
   linkedin = "",
 }) {
   return (
-    <div className="relative group  bg-[#FFE165] rounded-2xl border-8 border-[#FFE165] shadow-lg max-h-fit overflow-hidden">
-      <div className="bg-linear-to-t from-[#D3E1C3] to-[#CDC8C4]  rounded-xl px-2 py-4">
+    <div className="relative group  bg-[#FFE165] rounded-2xl border-8 border-[#FFE165] shadow-lg max-h-fit overflow-hidden w-full">
+      <div className="bg-linear-to-t from-[#D3E1C3] to-[#CDC8C4]  rounded-xl px-2 py-4 w-full">
         <img
           className="absolute z-50 right-0 top-0"
           src="/images/teams/hacksLogo.svg"
@@ -22,15 +22,16 @@ export default function HackCard({
           src="/images/teams/Stage.svg"
           alt=""
         />
-        <div className="rounded-md h-80 max-h-80 flex flex-col overflow-hidden border max-w-52 border-gray-300">
-          <img
+        <div className="rounded-md h-80 max-h-80 flex flex-col overflow-hidden border border-gray-300 w-full">
+          
+            <img
             // src="/images/teams/member.png"
             src={image}
             alt="photo"
-            className="w-auto flex-1 object-cover"
+            className="w-full flex-1 object-cover"
           />
-          <div className="flex absolute bottom-0 flex-col translate-y-[20%] group-hover:translate-y-[-16%] items-center bg-[#d3e1c3] gap-2 transition-transform duration-300">
-            <img src="/images/teams/border.svg" alt="" />
+          <div className="flex absolute w-full bottom-0 flex-col translate-y-[20%] group-hover:translate-y-[-16%] items-center bg-[#d3e1c3] gap-2 transition-transform duration-300">
+            <img className="w-full" src="/images/teams/border.svg" alt="" />
             <h2>{name}</h2>
             <h6>{domain}</h6>
             <span className=" gap-2 flex">
@@ -39,6 +40,9 @@ export default function HackCard({
               </a>
               <a href={github} className="hover:text-gray-900 text-gray-700 mr-2">
                 <img src="/images/teams/github.svg" className="w-4 h-4" alt="" />
+              </a>
+              <a href={codeforces} className="hover:text-gray-900 text-gray-700 pr-1">
+                <img src="/images/teams/codeforces.svg" className="w-4 h-4" alt="" />
               </a>
               <a href={facebook} className="hover:text-gray-900 text-gray-700">
                 <img src="/images/teams/facebook.svg" className="w-4 h-4" alt="" />
--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -2,62 +2,74 @@ import TeamCard from "../../Components/TeamCard/TeamCard";
 import "./Team.css";
 
 const Team = () => {
-  const imgSrc =
-    "https://tecnoesis.co.in/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdgnlmdkyq%2Fimage%2Fupload%2Fv1761037096%2FScreenshot_2025-10-19_230423_gspyiq.webp&w=640&q=75";
+    const imgSrc =
+        "https://tecnoesis.co.in/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdgnlmdkyq%2Fimage%2Fupload%2Fv1761037096%2FScreenshot_2025-10-19_230423_gspyiq.webp&w=640&q=75";
 
-  const members = Array(8).fill({
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    codeforces: "https://codeforces.com",
-    linkedin: "https://linkedin.com",
-    img: imgSrc,
-  });
+    const members = Array(8).fill({
+        github: "https://github.com",
+        facebook: "https://facebook.com",
+        codeforces: "https://codeforces.com",
+        linkedin: "https://linkedin.com",
+        img: imgSrc,
+    });
 
-  return (
-    <div className="relative flex flex-col justify-start items-center min-h-screen z-1 bg-[#D73055]">
-      <img
-        src="/images/teams/outer.svg"
-        alt="Pokédex frame"
-        className="absolute inset-0 w-full h-full object-contain pointer-events-none outer"
-      />
-      <div className="team-container z-5 overflow-hidden pointer-events-auto">
-        <div className="team-header">
-          <h1 className="team-title font-fire">Teams</h1>
-          <h2 className="team-subtitle">Core Members</h2>
-        </div>
+    return (
+        <>
+            <div className="relative flex flex-col justify-start items-center max-w-screen min-h-screen z-1 bg-[#A7D2F6] ">
+                <img
+                    src="/images/teams/upperBg.svg"
+                    alt="Pokédex frame"
+                    className="absolute inset-0 max-w-screen h-full object-contain pointer-events-none outer"
+                />
 
-        <div className="team-grid">
-          {members.slice(0, 8).map((member, index) => (
-            <TeamCard
-              key={index}
-              github={member.github}
-              facebook={member.facebook}
-              codeforces={member.codeforces}
-              linkedin={member.linkedin}
-              img={member.img}
-            />
-          ))}
-        </div>
+                <div style={{
+                    backgroundImage: `url("/images/teams/middleBg.svg")`
+                }} className="z-10 border-top bg-contain min-w-screen">
+                    <div className="team-container z-5 overflow-hidden pointer-events-auto">
+                        <div className="team-header">
+                            <h1 className="text-3xl font-pokemon ">Teams</h1>
+                            <h2 className="team-subtitle">Core Members</h2>
+                        </div>
 
-        <div className="team-header">
-          <h2 className="team-subtitle">Junior Members</h2>
-        </div>
+                        <div className="team-grid">
+                            {members.slice(0, 8).map((member, index) => (
+                                <TeamCard
+                                    key={index}
+                                    github={member.github}
+                                    facebook={member.facebook}
+                                    codeforces={member.codeforces}
+                                    linkedin={member.linkedin}
+                                    img={member.img}
+                                />
+                            ))}
+                        </div>
 
-        <div className="team-grid">
-          {members.slice(0, 8).map((member, index) => (
-            <TeamCard
-              key={index + 4}
-              github={member.github}
-              facebook={member.facebook}
-              codeforces={member.codeforces}
-              linkedin={member.linkedin}
-              img={member.img}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+                        <div className="team-header">
+                            <h2 className="team-subtitle">Junior Members</h2>
+                        </div>
+
+                        <div className="team-grid">
+                            {members.slice(0, 8).map((member, index) => (
+                                <TeamCard
+                                    key={index + 4}
+                                    github={member.github}
+                                    facebook={member.facebook}
+                                    codeforces={member.codeforces}
+                                    linkedin={member.linkedin}
+                                    img={member.img}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <img
+                    src="/images/teams/lowerBg.svg"
+                    alt="Pokédex frame"
+                    className="inset-0 min-w-screen h-full object-cover z-10 outer"
+                />
+            </div>
+        </>
+    );
 };
 
 export default Team;
