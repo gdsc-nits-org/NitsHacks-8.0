e9382f9c252ace36bf57a105129230031e107dfe
di35117 <dishankchoudhury@gmail.com>
2025-11-04 19:56:35 +0530

    Add AboutEvents to component exports

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -3,5 +3,5 @@ import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 import Faq from "./Faq/Faq";
 import Sponsor from "./Sponsors/Sponsors";
-
-export { Button, Navbar, Footer, Faq, Sponsor };
+import AbouEvents from "./events/aboutEvents";
+export { Button, Navbar, Footer, Faq, Sponsor, AboutEvents };
