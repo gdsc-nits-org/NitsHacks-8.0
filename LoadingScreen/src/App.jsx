be7a14fed47927860d388f6d155931fc0ff7462a
Swap-ux <swapnil_ug_24@ei.nits.ac.in>
2025-11-05 16:19:34 +0530

    Added Navbar and LoadingScreen

---
+++ LoadingScreen/src/App.jsx
@@ -0,0 +1,15 @@
+import  PokemonDashboard from "./components/Navigation.jsx"
+
+import LoadingScreen from "./components/loading.jsx"
+function App() {
+  
+
+  return (
+    <>
+ 
+   <PokemonDashboard/>
+    </>
+  )
+}
+
+export default App;
